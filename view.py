fbbe62cda479edc31d21bb46ed86117f9422e962
Saglara <nohaevass@yandex.ru>
2022-12-17 16:36:34 +0300

    Update view.py

--- view.py
+++ view.py
@@ -3,3 +3,6 @@ def view_data(data):
 
 def get_value():
     return bool(input("Добавить новый контракт (True or False)? "))
+
+def rasshiren():
+    return str(input('Выберите формат экспортируемых данных (txt или csv)? '))
