f62b24bb847533591602f80de7a6c1a7364730ab
Saglara <nohaevass@yandex.ru>
2022-12-17 16:36:44 +0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,3 +7,10 @@ def button_click():
     if value_a==True:
         result=model.add()
     view.view_data(result)
+
+def button_save_click():
+    rasshirenie=view.rasshiren()
+    if rasshirenie=='txt':
+        model.exsport_txt()
+    else:
+        model.exsport_csv()   
