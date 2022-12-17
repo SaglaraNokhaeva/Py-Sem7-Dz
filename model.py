c749d3303e570373a17723455f27a06a66cf3828
Saglara <nohaevass@yandex.ru>
2022-12-17 14:29:58 +0300

    Update model.py

--- model.py
+++ model.py
@@ -4,5 +4,22 @@ guide =[]
 for s in data.readlines():
     guide.append(s) 
 
-print(guide)
 data.close()
+print(guide)
+
+a=input("Добавить новый контракт (True or False)?")
+if a==True:
+    data = open('справочник.txt','a',encoding='UTF-8')
+    s1=input("Введите Фамилию")
+    guide.append(s1)
+    s1=input("Введите Имя")
+    guide.append(s1)
+    s1=input("Введите Телефон")
+    guide.append(s1)
+    s1=input("Введите Описание")
+    guide.append(s1)
+    data.write(guide+'\n')
+    data.close()
+
+print(guide)
+
