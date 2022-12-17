8369fe2fc060fe07e8e045cbeccbfe14878283f8
Saglara <nohaevass@yandex.ru>
2022-12-17 16:01:53 +0300

    Update model.py

--- model.py
+++ model.py
@@ -6,20 +6,27 @@ for s in data.readlines():
 data.close()
 print(guide)
 
-a=bool(input("Добавить новый контракт (True or False)?"))
-if a==True:
-    guide =[]
-    data1 = open('справочник.txt','a',encoding='UTF-8')
-    s1=input("Введите Фамилию: ")
-    guide.append(s1)
-    s1=input("Введите Имя: ")
-    guide.append(s1)
-    s1=input("Введите Телефон: ")
-    guide.append(s1)
-    s1=input("Введите Описание: ")
-    guide.append(s1)
-    data1.write("\n")
-    data1.write("\n".join(guide))
-    
-print(guide)
-data1.close()
+# a=bool(input("Добавить новый контракт (True or False)?"))
+# flag=bool
+def init (a):
+    global flag
+    flag=a
+
+def add():
+        guide =[]
+        data1 = open('справочник.txt','a',encoding='UTF-8')
+        s1=input("Введите Фамилию: ")
+        guide.append(s1)
+        s1=input("Введите Имя: ")
+        guide.append(s1)
+        s1=input("Введите Телефон: ")
+        guide.append(s1)
+        s1=input("Введите Описание: ")
+        guide.append(s1)
+        data1.write("\n")
+        data1.write("\n".join(guide))   
+        data1.close()
+        return guide
+        
+# if a==True:
+#     add()
